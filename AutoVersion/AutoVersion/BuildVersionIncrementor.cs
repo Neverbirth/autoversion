83a30d82dc18b3950a850bd39f568e4adea6e04b
neverbirth <neverbirth@c7f5c1c2-6a69-1386-0bfd-65faecd642be>
2010-04-22 11:01:13 +0000

    Initial commit.

---
+++ AutoVersion/AutoVersion/BuildVersionIncrementor.cs
@@ -0,0 +1,143 @@
+﻿using AutoVersion.Incrementors;
+
+using PluginCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace AutoVersion
+{
+    internal enum BuildAction : byte
+    {
+        Building,
+        Testing
+    }
+
+    internal class BuildVersionIncrementor : IDisposable
+    {
+        private BuildAction _currentBuildAction;
+        private PluginMain _pluginMain;
+        private ProjectItem _projectItem;
+        private DateTime _buildStartDate = DateTime.MinValue;
+
+        private IncrementorCollection _incrementors = new IncrementorCollection();
+        /// <summary>
+        /// Gets the incrementors.
+        /// </summary>
+        /// <value>The incrementors.</value>
+        public IncrementorCollection Incrementors
+        {
+            get { return _incrementors; }
+        }
+
+        private static BuildVersionIncrementor _instance;
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static BuildVersionIncrementor Instance
+        {
+            get { return _instance; }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BuildVersionIncrementor"/> class.
+        /// </summary>
+        /// <param name="pluginMain">The base instance.</param>
+        public BuildVersionIncrementor(PluginMain pluginMain)
+        {
+            _pluginMain = pluginMain;
+            _instance = this;
+        }
+
+        /// <summary>
+        /// Initializes the incrementors.
+        /// </summary>
+        public void InitializeIncrementors()
+        {
+            try
+            {
+                _incrementors.AddFrom(Assembly.GetExecutingAssembly());
+
+                string[] files = Directory.GetFiles(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "*.Incrementor.dll");
+
+                foreach (string file in files)
+                {
+                    Assembly asm = Assembly.LoadFrom(file);
+
+                    _incrementors.AddFrom(asm);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+                //                Logger.Write("Exception occured while initializing incrementors.\n" + ex.ToString(), LogLevel.Error);
+            }
+        }
+
+        private void ExecuteIncrement()
+        {
+            /*            if (!GlobalAddinSettings.Default.IsEnabled)
+                        {
+                            return;
+                        }*/
+
+            try
+            {
+                _projectItem = new ProjectItem();
+                UpdateProject();
+            }
+            catch (Exception ex)
+            {
+                throw;
+                //                Logger.Write("Error occured while executing build version increment.\n" + ex.ToString(), LogLevel.Error);
+            }
+        }
+
+        /// <summary>
+        /// Updates build version for the given project
+        /// </summary>
+        private void UpdateProject()
+        {
+            VersioningSettings settings = _projectItem.IncrementSettings;
+            if (settings.BuildAction == BuildActionType.Both ||
+                  (settings.BuildAction == BuildActionType.Build && _currentBuildAction == BuildAction.Building) ||
+                  (settings.BuildAction == BuildActionType.Testing && _currentBuildAction == BuildAction.Testing))
+            {
+                _projectItem.Version = _projectItem.IncrementSettings.VersioningStyle.Increment(_projectItem.Version,
+                                                                         _projectItem.IncrementSettings.IsUniversalTime ? DateTime.UtcNow : DateTime.Now,
+                                                                         _projectItem.IncrementSettings.StartDate);
+
+                _projectItem.SaveVersion();
+            }
+        }
+
+        public void OnBuilding(BuildAction action)
+        {
+            _currentBuildAction = action;
+            _buildStartDate = DateTime.Now;
+
+            ExecuteIncrement();
+        }
+
+        public void OnProject()
+        {
+            _projectItem = null;
+        }
+
+        #region  IDisposable
+
+        public void Dispose()
+        {
+            _projectItem = null;
+            _pluginMain = null;
+        }
+
+        #endregion
+    }
+}
---
+++ AutoVersion/AutoVersion/ProjectItem.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AutoVersion
+{
+    /// <summary>
+    /// The language type of the project
+    /// </summary>
+    internal enum LanguageType
+    {
+        /// <summary>
+        /// This is no work project
+        /// </summary>
+        None,
+        /// <summary>
+        /// ActionScript 3
+        /// </summary>
+        ActionScript3
+    }
+
+    /// <summary>
+    /// Class to wrap project items
+    /// </summary>
+    class ProjectItem
+    {
+
+        #region  Properties
+
+        public Version Version { get; set; }
+        public VersioningSettings IncrementSettings { get; private set; }
+        public LanguageType ProjectType { get; set; }
+
+        #endregion
+
+        #region  Constructor
+
+        public ProjectItem()
+        {
+            this.IncrementSettings = new VersioningSettings();
+            this.IncrementSettings.Load();
+            this.ProjectType = LanguageType.ActionScript3;
+            this.LoadVersion();
+        }
+
+        #endregion
+
+        #region  Methods
+
+        public string GetVersionFilename()
+        {
+            string basePath = Path.GetDirectoryName(PluginCore.PluginBase.CurrentProject.ProjectPath);
+            string filename = "Version.as";
+
+            if (!string.IsNullOrEmpty(IncrementSettings.VersionFilename))
+            {
+                filename = IncrementSettings.VersionFilename;
+                //                return Common.MakeAbsolutePath(basePath, solutionItem.IncrementSettings.AssemblyInfoFilename);
+            }
+
+            return Path.Combine(basePath, filename);
+        }
+
+        private string GetVersionDataValue(string versionData, string pattern)
+        {
+            try
+            {
+                return
+                    Regex.Matches(versionData, pattern)[0].Groups[1].Captures[0].Value;
+            }
+            catch (Exception ex)
+            {
+                return "0";
+            }
+        }
+
+        public void LoadVersion()
+        {
+            Version version;
+            string versionFile = GetVersionFilename();
+
+            if (!File.Exists(versionFile))
+            {
+                version = new Version(1, 0, 0, 0);
+            }
+            else
+            {
+                string fileVersionData = File.ReadAllText(versionFile);
+
+                string major = GetVersionDataValue(fileVersionData, @"static public const Major:int = (\d+);");
+                string minor = GetVersionDataValue(fileVersionData, @"static public const Minor:int = (\d+);");
+                string build = GetVersionDataValue(fileVersionData, @"static public const Build:int = (\d+);");
+                string revision = GetVersionDataValue(fileVersionData, @"static public const Revision:int = (\d+);");
+                
+                version = new Version(major + "." + minor + "." + build + "." + revision);
+            }
+
+            this.Version = version;
+        }
+
+        public void SaveVersion()
+        {
+            string versionFile = GetVersionFilename();
+            using (FileStream fileStream = new FileStream(versionFile, FileMode.Create, FileAccess.Write))
+            {
+                using (StreamWriter writer = new StreamWriter(fileStream, new UTF8Encoding(false)))
+                {
+                    writer.WriteLine("package ");
+                    writer.WriteLine("{");
+                    writer.WriteLine("	public final class Version");
+                    writer.WriteLine("	{");
+                    writer.WriteLine("		static public const Major:int = " + Version.Major.ToString() + ";");
+                    writer.WriteLine("		static public const Minor:int = " + Version.Minor.ToString() + ";");
+                    writer.WriteLine("		static public const Build:int = " + Version.Build.ToString() + ";");
+                    writer.WriteLine("		static public const Revision:int = " + Version.Revision.ToString() + ";");
+                    writer.WriteLine("	}");
+                    writer.WriteLine("}");
+                }
+            }
+        }
+
+        #endregion
+    }
+}
