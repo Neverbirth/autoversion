6d956418bd40c643d24e2e0cc8a06b8aa6c74a5d
neverbirth <neverbirth@c7f5c1c2-6a69-1386-0bfd-65faecd642be>
2010-04-24 17:29:59 +0000

    Correctly load and save start date. Correctly load and save version filename. Added version file package to set a custom package for the version class file.

---
+++ AutoVersion/AutoVersion/Design/FileNameEditorEx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms.Design;
+
+namespace AutoVersion.Design
+{
+    class FileNameEditorEx: FileNameEditor
+    {
+
+        protected override void InitializeDialog(System.Windows.Forms.OpenFileDialog openFileDialog)
+        {
+            base.InitializeDialog(openFileDialog);
+
+            openFileDialog.CheckFileExists = false;
+        }
+
+    }
+}
